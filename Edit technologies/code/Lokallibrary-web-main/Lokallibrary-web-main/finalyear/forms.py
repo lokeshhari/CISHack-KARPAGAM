d0d653193462f99e0724fc26ec034271cb3944b1
lokeahhari <lokeahhariv.p.m22@gmail.com>
2021-02-25 22:27:01 +0530

    Source code and presentation

---
+++ Edit technologies/code/Lokallibrary-web-main/Lokallibrary-web-main/finalyear/forms.py
@@ -0,0 +1,71 @@
+import datetime
+
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
+from django.forms import ModelForm
+from finalyear.models import Author,Book,Request
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.core.exceptions import ValidationError
+
+class CustomUserCreationForm(forms.Form):
+    username = forms.CharField(label='Enter Username', min_length=4, max_length=150)
+    email = forms.EmailField(label='Enter email')
+    password1 = forms.CharField(label='Enter password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data['username'].lower()
+        r = User.objects.filter(username=username)
+        if r.count():
+            raise  ValidationError("Username already exists")
+        c=0
+        for a in username:
+            if (a.isspace()):
+                c=1
+        if c:
+            raise ValidationError("Username shouldnot contain space")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        r = User.objects.filter(email=email)
+        if r.count():
+            raise  ValidationError("Email already exists")
+        return email
+
+    def clean_password2(self):
+        SpecialSym=['@','#','&','₹','_']
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+
+        if not any(char.isdigit() for char in password2):
+            raise ValidationError('Password should have at least one numeric characters') 
+
+        if not any(char.isupper() for char in password2):
+            raise ValidationError('Password should have at least one uppercase letter')
+
+        if not any(char.islower() for char in password2):
+            raise ValidationError('Password should have at least one lowercase letter')
+
+        if not any(char in SpecialSym for char in password2):
+            raise ValidationError('Password should have at least one of the symbols $@#') 
+
+       
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Password doesn't match")
+
+        return password2
+
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['email'],
+            self.cleaned_data['password1']
+        )
+        return user
+
+    
---
+++ Edit technologies/code/Lokallibrary-web-main/Lokallibrary-web-main/finalyear/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('authors/create/', views.AuthorCreate.as_view(), name='author_create'),
+    path('authors/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author_update'),
+    path('authors/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author_delete'),
+    path('books/create/', views.BookCreate.as_view(), name='book_create'),
+    path('books/<int:pk>/update/', views.BookUpdate.as_view(), name='book_update'),
+    path('books/<int:pk>/delete/', views.BookDelete.as_view(), name='book_delete'),
+    path('register/',views.register,name='register'),
+    path('results/', views.SearchView.as_view(), name='search'),
+    path('request/',views.book_request.as_view() , name='book_request'),
+    path('request-sended/<int:pk>',views.RequestDetailView.as_view(),name='request-detail'),
+]
